38d915fac0730effb003f0a8286af278fbd926d9
MATEKUNA <kuna.mate@parlament.hu>
2023-03-13 12:06:59 +0100

    defer loading of events, cant return response if deferred in loadEvents modified

--- frontend/src/components/EventsPage.js
+++ frontend/src/components/EventsPage.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { useLoaderData, defer, Await } from 'react-router-dom';
 import EventsList from './EventsList';
 
 const EventsPage = () => {
     const data = useLoaderData();     // events will be the data returned in app js loader. (data returned by loader function in this component, it was moved from app js)
     //useLoader can access the 'closest' loader data
     const events = data.events;
-
-   /*  if(data.isError) {
-        return <p> {data.message} </p>
-    } */
     
+    // now return is different because of the refered events -> cant return events={events}, need to use Await!
     return (
+        <Suspense fallback={<p style={{ textAlign: 'center'}}>Loading...</p>}>
+            <Await resolve={events}>
+                {loadedEvents => <EventsList events={loadedEvents} />}
+            </Await>
+        </Suspense>
+    );
+    /* return (
         <>
             <div>Events Page</div>
             <EventsList events={events} />
         
         </>
-    );
+    ); */
+    
 }
 
 export default EventsPage;
 
-export const loader = async () => {
+const loadEvents = async () => {
     const response = await fetch('http://localhost:8081/events');
 
     if(!response.ok) {
@@ -34,6 +39,13 @@ export const loader = async () => {
     else {
       //  const data = await response.json();
       //  return data.events;			// returned value in loader can be accessable in other components
-         return response;         // react-router supports returning the response without extracting manually
+         return response.json().events;         // react-router supports returning the response without extracting manually
     } 
+}
+
+export const loader = () => {
+    // pass all http request to defer, if there are more
+    return defer({
+        events: loadEvents()
+    });
 }
