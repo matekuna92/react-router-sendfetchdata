40821bff7285cdf8eb3c733fd263b97479fa7111
MATEKUNA <kuna.mate@parlament.hu>
2023-03-22 10:55:48 +0100

    authForm practise

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -12,6 +12,28 @@ const AuthForm = () => {
 
   return (
     <>
+        <Form method="POST" className={classes.form}>
+            <h1>{isLogin ? 'Login' : 'Create new user'}</h1>
+
+            <p>
+                <label htmlFor='email'>Email</label>
+                <input id='email' type='email' name='email' required />
+            </p>
+            <p>
+                <label htmlFor='password'>Password</label>
+                <input id='password' type='password' name='password' required />
+            </p>
+
+            <div className={classes.actions}>
+                <button type='button' onClick={switchAuthHandler}>{isLogin ? 'Create new user' : 'Login'}</button>
+                <button type='button'>Save</button>
+            </div>
+        </Form>
+    </>
+  );
+}
+  /* return (
+    <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
         <p>
@@ -31,6 +53,6 @@ const AuthForm = () => {
       </Form>
     </>
   );
-}
+}  }*/
 
 export default AuthForm;
