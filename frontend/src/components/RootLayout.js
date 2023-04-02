ba9b0bc383ba04a8525719537a293b1799d62f5f
matekuna92 <kunamate66@gmail.com>
2023-04-02 14:16:42 +0200

    delete token in settimeout, have to handle expiration instead of fix timeout

--- frontend/src/components/RootLayout.js
+++ frontend/src/components/RootLayout.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import MainNavigation from "./MainNavigation";
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
 
 const RootLayout = () => {
    // const navigation = useNavigation();
+    const token = useLoaderData();    // no need for useRouterLoaderData here, because it's the Component what renders for '/' path where the tokenLoader is used - same level
+    const submit = useSubmit();      // programatically submit a form - send Logout form after 1 hour, when the token expires
+
+    useEffect(() => {
+        if(!token) {
+            return;
+        }
+
+        setTimeout(() => {
+            console.log('token: ', token);
+            submit(null, { action: '/logout', method: 'POST' });
+        }, 5000);     // setTimeout expects milliseconds
+    }, [token, submit]);
 
     return (
         <>
